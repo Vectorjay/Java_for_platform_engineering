5e0c3139158bb9defd4713be55e69b18101386a0
Vector <mac@Vector-2.local>
2026-07-31 21:00:19 +0100

    employee profile

--- lesson02/EmployeeProfile.java
+++ lesson02/EmployeeProfile.java
@@ -1,8 +1,22 @@
-String Name = "Victor Onyia";
-String Role = "Platform Engineer";
-Int Experience = "7 years";
-Bolean Learning Java = "true"
-Double Salary = 2500000.00;
-Int Surname Initial = 0;
-String currentGoal = "Become proficient in Java and Spring Boot.";
-String dreamGoal = "Platform Engineering Manager"
+public class EmployeeProfile {
+    public static void main(String[] args) {
+        String name = "Victor Onyia";
+        String role = "Platform Engineer";
+        int experience = 7;
+        boolean learningJava = true;
+        double salary = 2500000.00;
+        char surnameInitial = 'O';
+        String currentGoal = "Become proficient in Java and Spring Boot.";
+        String dreamGoal = "Platform Engineering Manager";
+
+    System.out.println("Name: " + name);
+    System.out.println("Role: " + role);
+    System.out.println("Experience: " + experience + " years");
+    System.out.println("Learning Java: " + learningJava);
+    System.out.println("Salary: " + salary);
+    System.out.println("Surname Initial: " + surnameInitial);
+    System.out.println("Current Goal: " + currentGoal);
+    System.out.println("Dream Role: " + dreamGoal);
+    }
+}
+
