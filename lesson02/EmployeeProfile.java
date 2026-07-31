cfdeff591b4ba3026d776abbc3e9c8b172cc73d0
Vector <mac@Vector-2.local>
2026-07-31 20:14:40 +0100

    Lesson 2 - Variables and Data Types

---
+++ lesson02/EmployeeProfile.java
@@ -0,0 +1,8 @@
+String Name = "Victor Onyia";
+String Role = "Platform Engineer";
+Int Experience = "7 years";
+Bolean Learning Java = "true"
+Double Salary = 2500000.00;
+Int Surname Initial = 0;
+String currentGoal = "Become proficient in Java and Spring Boot.";
+String dreamGoal = "Platform Engineering Manager"
